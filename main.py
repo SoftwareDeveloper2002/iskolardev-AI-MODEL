719bc133d37593e862ac10240213216b28c1c42f
Robby Roda <robbyroda.00@gmail.com>
2025-08-20 22:32:00 +0800

    tanginang to

--- main.py
+++ main.py
@@ -230,4 +230,5 @@ def analyze_file():
         "total_price": total_price
     })
 if __name__ == "__main__":
-    app.run(debug=True, port=7500)
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 locally
+    app.run(host="0.0.0.0", port=port)
